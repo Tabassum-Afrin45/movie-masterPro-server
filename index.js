406b4d68fb814bce5a4200e399e65abb420d9062
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2026-02-15 01:43:28 +0600

    fix a minor error

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db('movies_db')
     const movieCollection = db.collection('movies')
 
@@ -125,7 +125,7 @@ async function run() {
       res.send(result)
     })
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
   finally {
